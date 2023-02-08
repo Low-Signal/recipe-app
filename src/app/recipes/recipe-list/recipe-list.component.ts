b69c9a172a55d84f887f7dfa4bed6c9b7b26dbcb
Low-Signal <daimeun@comcast.net>
2023-02-08 01:22:38 -0500

    Updated recipe component to show current recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  // Emits the recipe selected from the recipe list
+  @Output("selectedRecipe") selectedRecipeEmitter = new EventEmitter <Recipe> ();
+
   recipes: Recipe[] = [
-    new Recipe("Test Name", "Test Description", "https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/a019-jakubk-0033-quick-pasta-to-go.jpg?w=1200&h=1200&fit=clip&crop=default&dpr=1&q=75&vib=3&con=3&usm=15&cs=srgb&bg=F4F4F3&ixlib=js-2.2.1&s=66e7cea6b7919fb9e923049341a67c3e"),
-    new Recipe("Test Name", "Test Description", "https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/a019-jakubk-0033-quick-pasta-to-go.jpg?w=1200&h=1200&fit=clip&crop=default&dpr=1&q=75&vib=3&con=3&usm=15&cs=srgb&bg=F4F4F3&ixlib=js-2.2.1&s=66e7cea6b7919fb9e923049341a67c3e")
+    new Recipe("Pasta", "Cheesy pasta!", "https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/a019-jakubk-0033-quick-pasta-to-go.jpg?w=1200&h=1200&fit=clip&crop=default&dpr=1&q=75&vib=3&con=3&usm=15&cs=srgb&bg=F4F4F3&ixlib=js-2.2.1&s=66e7cea6b7919fb9e923049341a67c3e"),
+    new Recipe("Pepperoni Pizza", "Delicous pizza!", "https://upload.wikimedia.org/wikipedia/commons/1/10/Pepperoni_pizza.jpeg")
 
   ];
+
+  selectedRecipe(selectedRecipe: Recipe) {
+    this.selectedRecipeEmitter.emit(selectedRecipe);
+  }
 }
+
